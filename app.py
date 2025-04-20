227c2681c228e8bf71a298a8a6495c0b982de935
XB0DB1X0 <6531501126@lamduan.mfu.ac.th>
2025-04-20 22:08:23 +0700

    app.py

--- app.py
+++ app.py
@@ -10,23 +10,21 @@ import pickle
 import matplotlib.pyplot as plt
 from sklearn.datasets import make_blobs
 
-# load model
-with open('kmeans_model.pkl', 'rb') as f:
-    loaded_model = pickle.load (f)
+with open('kmeans_model.pkl','rb') as f:
+    loaded_model = pickle.load(f)
     
-# set the title
+st.set_page_config(page_title="k-Means Clustering App",layout="centered")
+
 st.title("K-means Clustering Visualizer by Simeown Todsawongwat")
-    
-# set the page config
-st.set_page_config(page_title="k-Means Clustering App", layout="centered")
 
-# load dataset
-X, _ = make_blobs(n_samples=300, centers=loaded_model.n_clusters, cluster_std=0.60, random_state=0)
+st.subheader("Example Data for Visualization")
+st.markdown("This demo uses example data (2D) to illustrate clustering results.")
 
-# predict using the loaded model
-y_kmeans = loaded_model. predict(X)
 
-# plotting
+X, _ =make_blobs(n_samples=300, centers=loaded_model.n_clusters, cluster_std=0.60, random_state=0)
+
+y_kmeans = loaded_model.predict(X)
+
 fig, ax = plt.subplots()
 scatter = ax.scatter(X[:, 0], X[:, 1], c=y_kmeans, cmap='viridis')
 ax.scatter(loaded_model.cluster_centers_[:, 0], loaded_model.cluster_centers_[:, 1], s=300, c='red')
